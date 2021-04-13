79897de4e22191c0171fa1af8fc0b3735b264a9e
milky-boi <dr.milojko@gmail.com>
2021-04-14 00:43:07 +0200

    write new functions, fix old ones

--- module/networks/randomize.py
+++ module/networks/randomize.py
@@ -1,50 +1,50 @@
 
-def create_n_samples_of_random_graph(g, pop_name, n_samples, FPS):
-    total = pd.DataFrame()
-    for i in range(n_samples):
-        df = pd.DataFrame()
+# def create_n_samples_of_random_graph(g, pop_name, n_samples, FPS):
+#     total = pd.DataFrame()
+#     for i in range(n_samples):
+#         df = pd.DataFrame()
       
-        random_multi_g = generate_random_multigraph(g)
-        random_weighted_g = convert_multigraph_to_weighted(random_multi_g, FPS)
+#         random_multi_g = generate_random_multigraph(g)
+#         random_weighted_g = convert_multigraph_to_weighted(random_multi_g, FPS)
         
-        df = graph_global_measures(random_weighted_g, 'graph_' + str(i))
+#         df = graph_global_measures(random_weighted_g, 'graph_' + str(i))
         
-        total = pd.concat([total, df], axis=1)  
+#         total = pd.concat([total, df], axis=1)  
     
-    graph_cols = [col for col in df if col.startswith('graph')]
+#     graph_cols = [col for col in df if col.startswith('graph')]
     
-    df = pd.DataFrame()
-    #df['median'] = total.loc[:, graph_cols].median(axis=1)
-    df[pop_name.replace('.gml', '') + '_mean_random'] = total.loc[:, graph_cols].mean(axis=1)
-    # df['std'] = total.loc[:, graph_cols].std(axis=1)
+#     df = pd.DataFrame()
+#     #df['median'] = total.loc[:, graph_cols].median(axis=1)
+#     df[pop_name.replace('.gml', '') + '_mean_random'] = total.loc[:, graph_cols].mean(axis=1)
+#     # df['std'] = total.loc[:, graph_cols].std(axis=1)
     
-    return df
-            else:
-                G[node_1][node_2]['count'] +=1
-                G[node_1][node_2]['duration'] = old_duration + new_duration
+#     return df
+#             else:
+#                 G[node_1][node_2]['count'] +=1
+#                 G[node_1][node_2]['duration'] = old_duration + new_duration
                 
-                count = G[node_1][node_2]['count']
-                duration = G[node_1][node_2]['duration']
+#                 count = G[node_1][node_2]['count']
+#                 duration = G[node_1][node_2]['duration']
                 
-                G[node_1][node_2]['frequency'] = float((count/duration)/0.5)
-                G[node_1][node_2]['averaging'] = float(1/599.5)*(float(duration/count)-0.5)
-                # created_edges +=1
+#                 G[node_1][node_2]['frequency'] = float((count/duration)/0.5)
+#                 G[node_1][node_2]['averaging'] = float(1/599.5)*(float(duration/count)-0.5)
+#                 # created_edges +=1
              
-        else:
-            duration = random.uniform(0.5, 600)
-            count = 1  
+#         else:
+#             duration = random.uniform(0.5, 600)
+#             count = 1  
             
-            frequency = float((count/duration)/0.5)
-            averaging = float(1/599.5)*(float(duration/count)-0.5)
+#             frequency = float((count/duration)/0.5)
+#             averaging = float(1/599.5)*(float(duration/count)-0.5)
           
-            G.add_edge(node_1, node_2,
-                    duration=duration,
-                    count=count,
-                    frequency=frequency,
-                    averaging=averaging)
-            # created_edges +=1       
+#             G.add_edge(node_1, node_2,
+#                     duration=duration,
+#                     count=count,
+#                     frequency=frequency,
+#                     averaging=averaging)
+#             # created_edges +=1       
             
-    return G
+#     return G
 
 
 def generate_random_multigraph(g):
