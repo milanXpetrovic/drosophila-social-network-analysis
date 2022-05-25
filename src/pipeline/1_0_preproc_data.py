7797ac526864aa72b17187d74643963675d7c72e
milanxpetrovic <milan.petrovic@uniri.hr>
2022-05-25 15:32:07 +0200

    finish angle betwen individualz

--- src/pipeline/1_0_preproc_data.py
+++ src/pipeline/1_0_preproc_data.py
@@ -1,10 +1,11 @@
 
 # %%
-import pandas as pd
-import my_module as mm
-import sys
+import math
+import numpy as np
 import yaml
-
+import sys
+import my_module as mm
+import pandas as pd
 sys.path.append('/home/milanpetrovic/my_module/src')
 
 
@@ -22,11 +23,46 @@ for file_name, file_path in files_to_check.items():
     df = pd.read_csv(file_path)
     df = mm.interpolate_inf_values(df, interpolation_method='linear')
 
+    df = mm.convert_radians_to_degrees(df, config['angle_column'])
+
     name = SAVE_PATH + '/' + config['pop_name'] + '/' + file_name
     df.to_csv(name)
 
+# %%
 
+PATH = '/home/milanpetrovic/my_module/data/preproc/1_0_preproc_data/25_03_2022/2022-03-25_11-46_fly_1.csv'
+
+df = pd.read_csv(PATH).round(decimals=2)
+df = df[['ANGLE', 'X (cm)', 'Y (cm)']]
+df.reset_index(inplace=True)
+
+
+df = df.iloc[:-1, :]
+df = df[['X (cm)', 'Y (cm)']]
+df.columns = ['x', 'y']
+# df1.reset_index()
+
+res = df
+
+res['x_axis_dif'] = (0 - res['x']).abs()
+res['y_axis_dif'] = (0 - res['y']).abs()
+
+res['distance'] = np.sqrt(np.square(res['x_axis_dif']) +
+                          np.square(res['y_axis_dif']))
+
+res['angle'] = np.sin(res['y_axis_dif']/res['distance'])
+
+res = mm.convert_radians_to_degrees(res, 'angle')
+
+chck = res.iloc[::100,:]
+# %%
+sys.path.append('/home/milanpetrovic/my_module/src')
+
+
+# ax = angles.plot.hist()
 # preproc_data(raw_data):
 # inspec_raw_data():
 # FOOS FOR DATA PRE PROCESING
 # NORMALIZE, ROUND VALUES, ETC.
+
+# %%
--- src/pipeline/2_0_process_data.py
+++ src/pipeline/2_0_process_data.py
@@ -3,55 +3,28 @@
 # get_path_features(raw_data):
 # %%
 
-sys.path.append('/home/milanpetrovic/my_module/src')
-import pandas as pd
-import my_module as mm
-import sys
+import numpy as np
 import yaml
-import numpy as np 
+import sys
+sys.path.append('/home/milanpetrovic/my_module/src')
+import my_module as mm
+import pandas as pd
 
 CONFIG = '/home/milanpetrovic/my_module/configs/main.yaml'
+PREPROC_DATA = '/home/milanpetrovic/my_module/data/preproc/1_0_preproc_data/25_03_2022'
+SAVE_PATH = '/home/milanpetrovic/my_module/data/preproc/2_0_process_data/'
 
 with open(CONFIG) as f:
     config = yaml.safe_load(f)
 
-files_to_check = mm.load_files_from_folder(
-    config['raw_data_path'], config['file_extension'])
+x_col_name = config['x_col_name']
+y_col_name = config['y_col_name']
+file_extension = config['file_extension']
 
+# distances_in_time = mm.distance_in_time_for_individuals(
+#     PREPROC_DATA, x_col_name, y_col_name, file_extension)
 
-files = []
-for n,p in files_to_check.items():
-    df = pd.read_csv(p)
-    files.append(df)
-
-
-final_df = pd.DataFrame()
-
-for i in range(len(files)):
-
-    df1 = files[i]
-
-    next_flie = i + 1
-
-    if next_flie <= len(files):
-        for j in range(next_flie, len(files)):
-            df2 = files[j]
-
-            df = pd.concat([df1['X#wcentroid (cm)'], df1['Y#wcentroid (cm)'],
-                            df2['X#wcentroid (cm)'], df2['Y#wcentroid (cm)']], axis=1)
-
-            df.columns = ['pos_x1', 'pos_y1', 'pos_x2', 'pos_y2']
-
-            df['x_axis_dif'] = (df['pos_x1'] - df['pos_x2']).abs()
-            df['y_axis_dif'] = (df['pos_y1'] - df['pos_y2']).abs()
-
-            name = str(i+1) + ' ' + str(j+1)
-            final_df[name] = np.sqrt(np.square(df['x_axis_dif']) +
-                                        np.square(df['y_axis_dif']))
-
-
-
-
-
+# distances_in_time = distances_in_time.round(decimals=2)
+# distances_in_time.to_csv(SAVE_PATH + 'distances_in_time.csv')
 
 
