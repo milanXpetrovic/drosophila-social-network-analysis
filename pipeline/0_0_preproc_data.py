af95a6ae6b3d193dac40169cf04a10406f3faf71
milky-boi <dr.milojko@gmail.com>
2023-05-03 20:12:05 +0000

    refractor: updated function calls and variable names

--- pipeline/0_0_preproc_data.py
+++ pipeline/0_0_preproc_data.py
@@ -1,33 +1,29 @@
+# %%
 import os
 import pandas as pd
-import package_functions as hf
 
-EXPERIMENT_DURATION = 600  # experiment duration time must be in seconds
-FPS = 24
-DATAFRAME_LEN = EXPERIMENT_DURATION * FPS
+from src import settings
+from src.utils import fileio, data_utils
 
-DATA_PATH = r"F:/0_fax/DM_dataset/raw_trackings_pop"
-SAVE_PATH = "../2_pipeline/0_0_preproc_data/out"
 
-experiments = hf.load_multiple_folders(DATA_PATH)
+SCRIPT_OUTPUT = os.path.join(
+    settings.OUTPUT_DIR, os.path.basename(__file__).replace(".py", "")
+)
 
-for pop_name, path in experiments.items():
-    if not hf.check_data(path):
-        continue
+treatment = fileio.load_multiple_folders(settings.INPUT_DIR)
 
-    if not os.path.exists(SAVE_PATH + "/" + pop_name):
-        os.mkdir(SAVE_PATH + "/" + pop_name)
+os.makedirs(SCRIPT_OUTPUT, exist_ok=True)
 
-    fly_dict = hf.load_files_from_folder(path)
+for group_name, group_path in treatment.items():
+    os.makedirs(os.path.join(SCRIPT_OUTPUT, group_name), exist_ok=True)
 
-    # hf.inspect_population_coordinates(path, pop_name)
+    fly_dict = fileio.load_files_from_folder(group_path)
+    min_x, min_y = data_utils.find_group_mins(group_path)
 
-    min_x, min_y = hf.find_pop_mins(path)
+    for fly_name, fly_path in fly_dict.items():
+        df = pd.read_csv(fly_path)
+        df = df.head(settings.DATAFRAME_LEN)
+        df = data_utils.prepproc(df, min_x, min_y)
+        df = data_utils.round_coordinates(df, decimal_places=0)
 
-    for fly_name, path in fly_dict.items():
-        df = pd.read_csv(path, index_col=0)
-        df = df.head(DATAFRAME_LEN)
-        df = hf.prepproc(df, min_x, min_y)
-        df = hf.round_coordinates(df, decimal_places=0)
-
-        df.to_csv(SAVE_PATH + "/" + pop_name + "/" + fly_name)
+        df.to_csv(os.path.join(SCRIPT_OUTPUT, group_name, fly_name))
