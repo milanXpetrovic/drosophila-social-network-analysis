a9b288f044854a37b7ae8bc4d4f62d2d1f57e6f1
milky-boi <dr.milojko@gmail.com>
2023-05-26 14:28:58 +0000

    using env variable for Treatment value

--- pipeline/0_2_angles_between_flies_matrix.py
+++ pipeline/0_2_angles_between_flies_matrix.py
@@ -4,7 +4,7 @@ import sys
 from src import settings
 from src.utils import fileio, data_utils
 
-TREATMENT = sys.argv[1]
+TREATMENT = os.environ["TREATMENT"]
 
 SCRIPT_OUTPUT = os.path.join(settings.OUTPUT_DIR, TREATMENT, "0_2_angles_between_flies_matrix")
 os.makedirs(SCRIPT_OUTPUT, exist_ok=True)
