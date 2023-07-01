9414366c14c5d63249eb034cf16b4b8e1f5e367a
milanXpetrovic <milan.petrovic@uniri.hr>
2023-07-01 14:06:24 +0000

    added AREAN_DIAMETER variable

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import os
 
-
+ARENA_DIAMETER = 60  # in mm
 EXPERIMENT_DURATION = 1800  # experiment duration time must be in seconds
 FPS = 24
 
@@ -9,13 +9,11 @@ ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 INPUT_DIR = os.path.join(ROOT_DIR, "data", "trackings")
 OUTPUT_DIR = os.path.join(ROOT_DIR, "data", "processed")
 
+NORMALIZATION_DIR = os.path.join(ROOT_DIR, "data", "normalization")
+
 PIPELINE_DIR = os.path.join(ROOT_DIR, "pipeline")
 RESULTS_DIR = os.path.join(ROOT_DIR, "data", "results")
 
 LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 CONFIG_DIR = os.path.join(ROOT_DIR, "configs")
-
-
-
-
